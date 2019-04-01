21a88f9f656fb20bb4382b4577e249955bb00832
Theo Turner <theo_turner@outlook.com>
2019-04-01 17:35:23 +0100

    Fix stat error

--- install.sh
+++ install.sh
@@ -14,8 +14,8 @@ trap 'error_report $LINENO' ERR
 
 git clone https://github.com/OutlierVentures/ANVIL.git
 cp demo.ipynb ./ANVIL/anvil/demo.ipynb
-cp -a /charging_service/. ./ANVIL/anvil/charging_service
-cp -a /img/. ./ANVIL/anvil/img
+cp -a ./charging_service/. ./ANVIL/anvil/charging_service
+cp -a ./img/. ./ANVIL/anvil/img
 cd ANVIL
 ./scripts/install.sh
 pip3 install jupyter jupyterthemes

2adc3700e6a0ee5674a85dc329c9716e863cdd1e
Theo Turner <theo_turner@outlook.com>
2019-04-04 18:18:40 +0100

    Installer updates

--- install.sh
+++ install.sh
@@ -12,12 +12,6 @@ error_report() {
 }
 trap 'error_report $LINENO' ERR
 
-if [ "`whoami`" != "root" ]; then 
-	echo "Please run this script with superuser privileges (e.g. 'sudo ./setup.sh')."
-	exit 1 
-fi
-
-cd /root
 git clone https://github.com/OutlierVentures/ANVIL.git
 cp demo.ipynb ./ANVIL/anvil/demo.ipynb
 cp -a ./charging_service/. ./ANVIL/anvil/charging_service
@@ -26,14 +20,5 @@ cd ANVIL
 ./scripts/install.sh
 pip3 install jupyter jupyterthemes
 jt -t oceans16
-
-jupyter notebook --generate-config
-
-echo "c.NotebookApp.allow_root = True
-c.NotebookApp.certfile = u'$PEM_FILE_PATH'
-c.NotebookApp.ip = '0.0.0.0'
-c.NotebookApp.keyfile = u'$KEY_FILE_PATH'
-c.NotebookApp.open_browser = False
-c.NotebookApp.port = 443" >> /root/.jupyter/jupyter_notebook_config.py
 
 echo -e "${ongreen}ANVIL-Live installed.${endcolor}"
